af6d6466aa12c271484312d4c4a5cba7a59b1f5d
adamgwozdz00 <adamgwozdz00@gmail.com>
2022-07-08 00:40:28 +0200

    fixes

--- detect.sh
+++ detect.sh
@@ -1,1 +1,1 @@
-python yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 480 --conf 0.5 --source application/resources/images/image.jpg
+python3 yolov5/detect.py --weights yolov5/runs/train/exp/weights/best.pt --img 480 --conf 0.5 --source application/resources/images/image.jpg
